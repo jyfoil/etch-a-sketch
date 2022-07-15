6562ccd7c4966a9e0281f4eb6335ada501232c4f
Justin Yang <yjust16@gmail.com>
2022-07-15 16:57:17 -0700

    Add button to change grid size

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-const grid = document.querySelector(".grid");
-
 function makeGrid(numOfRows) {
+  const grid = document.querySelector(".grid");
   const total = numOfRows * numOfRows;
   grid.style.gridTemplateColumns = `repeat(${numOfRows}, 1fr)`;
 
   for (let i = 0; i < total; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    grid.appendChild(row);
+    const rows = document.createElement("div");
+    rows.classList.add("row");
+    grid.appendChild(rows);
 
-    row.addEventListener("mouseover", () => {
-      row.style.backgroundColor = "black";
+    rows.addEventListener("mouseover", () => {
+      rows.style.backgroundColor = "black";
     });
   }
 }
 
 makeGrid(16);
+
+function changeGrid() {
+  let newSize = prompt("Please enter the number of squares per side (1-100)");
+  makeGrid(newSize);
+}
+
+function resetGrid() {
+  const grid = document.querySelector(".grid");
+  const rows = document.querySelectorAll(".row");
+  rows.forEach((row) => {
+    grid.removeChild(row);
+  });
+}
+
+const size = document.querySelector(".size");
+size.addEventListener("click", () => {
+  resetGrid();
+  changeGrid();
+});
